e31aae818103ed8fdbfafe164e84b383c8ac6d61
adam <adam.jedrychowski10@gmail.com>
2022-03-16 21:38:34 +0100

    Color bar created

--- Lab_02/Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02/Lab_02.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
 const int XWindowSize = 750, YWindowSize = 600;
 
 
@@ -7,14 +8,12 @@ class BufferWindow : public sf::Drawable
 {
 private:
     sf::VertexArray ColorBar;
-    sf::CircleShape HSL, _HSV, CMY, RGB;
-    sf::VertexArray PointerColor[2];
+    sf::VertexArray HSL, _HSV, CMY, RGB;
     sf::RenderTexture ActualWindow;
-
-
     sf::Color ShadeOfCircles;
-    float ColorPointerY = 135;
 public:
+    sf::VertexArray PointerColor[2];
+    bool HoldingMouseButton = false;
     BufferWindow();
     
     virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const
@@ -22,11 +21,49 @@ public:
         target.draw(sf::Sprite(ActualWindow.getTexture()), states);
     }
 
-    void ChangeShade(sf::Event &event)
-    {
-        if(event.type==sf::Mouse::) float temp = event.mouseMove.y;
+    sf::RenderTexture& getActualWindow() { return ActualWindow; }
 
-        ColorPointerY;
+    void ChangeShade(sf::Event &event, sf::RenderWindow &window)
+    {
+        int temp, ChangeShade;
+        if (HoldingMouseButton) temp = event.mouseMove.y;
+        else temp = event.mouseButton.y;
+
+        //Changing position of Pointer bar
+        PointerColor[0].operator[](0).position.y = temp;
+        PointerColor[0].operator[](1).position.y = temp;
+        PointerColor[1].operator[](0).position.y = temp;
+        PointerColor[1].operator[](1).position.y = temp;
+
+        if (!HoldingMouseButton)
+        {
+            ActualWindow.clear(sf::Color::White);
+
+            ActualWindow.draw(PointerColor[0]);
+            ActualWindow.draw(PointerColor[1]);
+            ActualWindow.draw(HSL);
+            ActualWindow.draw(_HSV);
+            ActualWindow.draw(CMY);
+            ActualWindow.draw(RGB);
+            ActualWindow.draw(ColorBar);
+            ActualWindow.display();
+        }
+        else
+        {
+            ActualWindow.clear(sf::Color::White);
+
+            ActualWindow.draw(HSL);
+            ActualWindow.draw(_HSV);
+            ActualWindow.draw(CMY);
+            ActualWindow.draw(RGB);
+            ActualWindow.draw(ColorBar);
+            ActualWindow.draw(PointerColor[1]);
+            ActualWindow.draw(PointerColor[0]);
+            ActualWindow.display();
+        }
+
+        ChangeShade = (280 - temp) * 255 / 270.;
+        //cos dopisac
     }
 };
 
@@ -51,8 +88,32 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) window.close();
+            if (event.mouseButton.x > 610 && event.mouseButton.x < 650 && event.mouseButton.y>10 && event.mouseButton.y < 280)
+            {
+                if (!render.HoldingMouseButton && event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+                {
+                    render.ChangeShade(event, window);
+                    render.HoldingMouseButton = true;
+                }
+                else if (render.HoldingMouseButton && event.type == sf::Event::MouseButtonReleased)
+                {
+                    render.HoldingMouseButton = false;
+                    render.ChangeShade(event, window);
+                }
+            }
+            else if (render.HoldingMouseButton && event.mouseMove.x > 610 && event.mouseMove.x < 650 && event.mouseMove.y>10 && event.mouseMove.y < 280)
+            {
+                render.ChangeShade(event, window);
+            }
+
         }
         //tu wyrysowaæ wszystko na ekran
+        
+
+
+
+        render.getActualWindow().display();
+        window.draw(render);
 
         //tu wypisaæ na ekran wartoœæ FPS
         if (clock.getElapsedTime().asSeconds() >= 1.0f)
@@ -62,15 +123,14 @@ int main()
             frame_counter = 0;
         }
         frame_counter++;
-
-        window.draw(render);
         window.display();
     }
 }
 
 
 
-BufferWindow::BufferWindow() : ColorBar{ sf::Quads, 4 }
+BufferWindow::BufferWindow() : ColorBar{ sf::Quads, 4 }, HSL{sf::TriangleFan, 511}, _HSV{ sf::TriangleFan, 511 },
+CMY{ sf::TriangleFan, 511 }, RGB{ sf::TriangleFan, 511 }
 {
     ActualWindow.create(XWindowSize, YWindowSize);
     ColorBar[0].position = sf::Vector2f(610, 10);
@@ -84,39 +144,31 @@ BufferWindow::BufferWindow() : ColorBar{ sf::Quads, 4 }
     ColorBar[3].color = sf::Color::White;
 
 
-
-    HSL.setPosition(sf::Vector2f(30, 30));
-    HSL.setRadius(120);
-    HSL.setFillColor(sf::Color::Black);
-
-    _HSV.setPosition(sf::Vector2f(320, 30));
-    _HSV.setRadius(120);
-    _HSV.setFillColor(sf::Color::Black);
-
-    CMY.setPosition(sf::Vector2f(30, 320));
-    CMY.setRadius(120);
-    CMY.setFillColor(sf::Color::Black);
-
-    RGB.setPosition(sf::Vector2f(320, 320));
-    RGB.setRadius(120);
-    RGB.setFillColor(sf::Color::Black);
+    HSL[0].position = sf::Vector2f(150, 150);
+    _HSV[0].position = sf::Vector2f(440, 150);
+    CMY[0].position = sf::Vector2f(150, 440);
+    RGB[0].position = sf::Vector2f(440, 440);
+    for (int i = 1; i < 511; i++)
+    {
+        HSL[i].position = sf::Vector2f(150+120*cos(1.42*i), 150);
+    }
 
 
 
     PointerColor[0].setPrimitiveType(sf::Lines);
     PointerColor[0].resize(2);
-    PointerColor[0].operator[](0).position = sf::Vector2f(600, ColorPointerY);
+    PointerColor[0].operator[](0).position = sf::Vector2f(600, 135);
     PointerColor[0].operator[](0).color = sf::Color::Black;
 
-    PointerColor[0].operator[](1).position = sf::Vector2f(607, ColorPointerY);
+    PointerColor[0].operator[](1).position = sf::Vector2f(607, 135);
     PointerColor[0].operator[](1).color = sf::Color::Black;
 
     PointerColor[1].setPrimitiveType(sf::Lines);
     PointerColor[1].resize(2);
-    PointerColor[1].operator[](0).position = sf::Vector2f(653, ColorPointerY);
+    PointerColor[1].operator[](0).position = sf::Vector2f(653, 135);
     PointerColor[1].operator[](0).color = sf::Color::Black;
 
-    PointerColor[1].operator[](1).position = sf::Vector2f(660, ColorPointerY);
+    PointerColor[1].operator[](1).position = sf::Vector2f(660, 135);
     PointerColor[1].operator[](1).color = sf::Color::Black;
 
 
