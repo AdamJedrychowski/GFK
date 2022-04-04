ad002a767a042161dd69e99c6286bf95a186c747
adam <adam.jedrychowski10@gmail.com>
2022-04-04 20:42:29 +0200

    task on 5th lab

---
+++ Lab_05/GUIMyFrame1.cpp
@@ -0,0 +1,103 @@
+#include "GUIMyFrame1.h"
+#include <vector>
+#include <fstream>
+#include "vecmat.h"
+
+struct Point {
+ float x, y, z;
+ Point(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+ operator wxPoint();
+};
+
+struct Color {
+ int R, G, B;
+ Color(int _R, int _G, int _B) : R(_R), G(_G), B(_B) {}
+};
+
+struct Segment {
+ Point begin, end;
+ Color color;
+ Segment(Point _begin, Point _end, Color _color) : begin(_begin), end(_end), color(_color) {}
+};
+
+std::vector<Segment> data;
+
+GUIMyFrame1::GUIMyFrame1( wxWindow* parent )
+:
+MyFrame1( parent )
+{
+ m_button_load_geometry->SetLabel(_("Wczytaj Geometri\u0119"));
+ m_staticText25->SetLabel(_("Obr\u00F3t X:"));
+ m_staticText27->SetLabel(_("Obr\u00F3t Y:"));
+ m_staticText29->SetLabel(_("Obr\u00F3t Z:"));
+
+ WxSB_TranslationX->SetRange(0, 200); WxSB_TranslationX->SetValue(100);
+ WxSB_TranslationY->SetRange(0, 200); WxSB_TranslationY->SetValue(100);
+ WxSB_TranslationZ->SetRange(0, 200); WxSB_TranslationZ->SetValue(100);
+
+ WxSB_RotateX->SetRange(0, 360); WxSB_RotateX->SetValue(0);
+ WxSB_RotateY->SetRange(0, 360); WxSB_RotateY->SetValue(0);
+ WxSB_RotateZ->SetRange(0, 360); WxSB_RotateZ->SetValue(0);
+
+ WxSB_ScaleX->SetRange(1, 200); WxSB_ScaleX->SetValue(100);
+ WxSB_ScaleY->SetRange(1, 200); WxSB_ScaleY->SetValue(100);
+ WxSB_ScaleZ->SetRange(1, 200); WxSB_ScaleZ->SetValue(100);
+}
+
+void GUIMyFrame1::WxPanel_Repaint( wxUpdateUIEvent& event )
+{
+ Repaint();
+}
+
+void GUIMyFrame1::m_button_load_geometry_click( wxCommandEvent& event )
+{
+ wxFileDialog WxOpenFileDialog(this, wxT("Choose a file"), wxT(""), wxT(""), wxT("Geometry file (*.geo)|*.geo"), wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+
+ if (WxOpenFileDialog.ShowModal() == wxID_OK)
+ {
+  double x1, y1, z1, x2, y2, z2;
+  int r, g, b;
+
+  std::ifstream in(WxOpenFileDialog.GetPath().ToAscii());
+  if (in.is_open())
+  {
+   data.clear();
+   while (!in.eof())
+   {
+    in >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> r >> g >> b;
+    data.push_back(Segment(Point(x1, y1, z1), Point(x2, y2, z2), Color(r, g, b)));
+   }
+   in.close();
+  }
+ }
+}
+
+void GUIMyFrame1::Scrolls_Updated( wxScrollEvent& event )
+{
+WxST_TranslationX->SetLabel(wxString::Format(wxT("%g"), (WxSB_TranslationX->GetValue() - 100) / 50.0));
+WxST_TranslationY->SetLabel(wxString::Format(wxT("%g"), (WxSB_TranslationY->GetValue() - 100) / 50.0));
+WxST_TranslationZ->SetLabel(wxString::Format(wxT("%g"), (WxSB_TranslationZ->GetValue() - 100) / 50.0));
+
+WxST_RotateX->SetLabel(wxString::Format(wxT("%d"), WxSB_RotateX->GetValue()));
+WxST_RotateY->SetLabel(wxString::Format(wxT("%d"), WxSB_RotateY->GetValue()));
+WxST_RotateZ->SetLabel(wxString::Format(wxT("%d"), WxSB_RotateZ->GetValue()));
+
+WxST_ScaleX->SetLabel(wxString::Format(wxT("%g"), WxSB_ScaleX->GetValue() / 100.0));
+WxST_ScaleY->SetLabel(wxString::Format(wxT("%g"), WxSB_ScaleY->GetValue() / 100.0));
+WxST_ScaleZ->SetLabel(wxString::Format(wxT("%g"), WxSB_ScaleZ->GetValue() / 100.0));
+
+
+Repaint();
+}
+
+
+void GUIMyFrame1::Repaint()
+{
+    wxClientDC dc1(WxPanel);
+    wxBufferedDC dc(&dc1);
+    for (int i = 0; i < data.size(); i++)
+    {
+        dc.SetPen(wxPen(data[i].color.R, data[i].color.G, data[i].color.B));
+        dc.DrawLine(data[i].begin, data[i].end);
+    }
+}
