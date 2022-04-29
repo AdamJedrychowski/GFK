16e6d03e95b770a4364304a3682da972457808cc
adam <adam.jedrychowski10@gmail.com>
2022-04-29 17:55:37 +0200

    meta data

--- Lab_08/GUIMyFrame1.cpp
+++ Lab_08/GUIMyFrame1.cpp
@@ -10,6 +10,7 @@ MyFrame1( parent )
 void GUIMyFrame1::wxButton_ReadFile( wxCommandEvent& event )
 {
 // TODO: Implement wxButton_ReadFile
+<<<<<<< HEAD
 	wxFileDialog OpenFileDialog(this, _("Choose a file"), _(""), _(""), _("Obraz JPEG (*.jpg)|*.jpg"), wxFD_OPEN);
 	if (OpenFileDialog.ShowModal() == wxID_CANCEL) return;
 
@@ -54,6 +55,30 @@ void GUIMyFrame1::wxButton_ReadFile( wxCommandEvent& event )
 
 	//	FreeImage_Unload(bitmap);
 	//}
+=======
+	wxFileDialog OpenFileDialog(this, _("Choose a file"), _(""), _(""), _("(*.jpg)|*.jpg"), wxFD_OPEN);
+
+	if (OpenFileDialog.ShowModal() == wxID_CANCEL) return;
+
+	FIBITMAP* bitmap = FreeImage_Load(FIF_JPEG, OpenFileDialog.GetPath(), JPEG_DEFAULT);
+	if (!bitmap) return;
+
+	FITAG* tag = NULL;
+	FIMETADATA* mdhandle = NULL;
+	mdhandle = FreeImage_FindFirstMetadata(FIMD_EXIF_MAIN, bitmap, &tag);
+	
+	m_richText1->AppendText("Rozmiar obrazka: " + std::to_string(FreeImage_GetWidth(bitmap))
+		+ "*" + std::to_string(FreeImage_GetHeight(bitmap)) + "\nEXIF Info:\n");
+	if (mdhandle) {
+		do {
+			m_richText1->AppendText(std::string(FreeImage_GetTagKey(tag)) + ": " +
+				std::string(FreeImage_TagToString(FIMD_EXIF_MAIN, tag)) + "\n");
+		} while (FreeImage_FindNextMetadata(mdhandle, &tag));
+	}
+	FreeImage_FindCloseMetadata(mdhandle);
+
+
+>>>>>>> meta data
 }
 
 void GUIMyFrame1::wxButton_Censure( wxCommandEvent& event )
